25819f21bc8eae896290f41fb8852c9866ea392d
Ayesha Iftikhar <ayesha.iftikhar@venturedive.com>
2018-10-18 00:26:37 +0500

    Added /dist to gitignore file and removed public folder

--- dist/5.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{205:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.default=void 0;var u,a=(u=n(2))&&u.__esModule?u:{default:u};var d=function(){return a.default.createElement("div",null,"Reservation Page....")};t.default=d}}]);
