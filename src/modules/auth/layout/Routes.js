92996049254166b8156c780125b512ec6c47993b
Ayesha Iftikhar <ayesha.iftikhar@venturedive.com>
2018-10-19 02:11:52 +0500

    Added react-transition-group to animate mounting and unmounting of components

--- src/modules/auth/layout/Routes.js
+++ src/modules/auth/layout/Routes.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { Switch, Route } from 'react-router-dom';
+// Styled Components
+import styled from 'styled-components';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+// Code Splitting
 import Loadable from 'react-loadable';
 import PageLoader from '../../common/PageLoader';
 
@@ -30,22 +34,40 @@ const NoMatchPage = Loadable({
   loading: PageLoader,
 });
 
-const Routes = ({ match }) => {
+const Transition = styled(TransitionGroup)`
+  & .fade-enter {
+    opacity: 0;
+  }
+  & .fade-enter.fade-enter-active {
+    opacity: 1;
+    transition: opacity 500ms ease-in;
+  }
+  & .fade-exit {
+    opacity: 0;
+  }
+`;
+
+const Routes = ({ match, location }) => {
   return (
-    <Switch>
-      <Route exact path={`${match.url}`} component={MenuPage} />
-      <Route path={`${match.url}/menu`} component={MenuPage} />
-      <Route path={`${match.url}/pricing`} component={PricingPage} />
-      <Route path={`${match.url}/reservation`} component={ReservationPage} />
-      <Route path={`${match.url}/about`} component={AboutPage} />
-      <Route path={`${match.url}/contact`} component={ContactPage} />
-      <Route component={NoMatchPage} />
-    </Switch>
+    <Transition>
+      <CSSTransition key={location.key} classNames="fade" timeout={300}>
+        <Switch>
+          <Route exact path={`${match.url}`} component={MenuPage} />
+          <Route path={`${match.url}/menu`} component={MenuPage} />
+          <Route path={`${match.url}/pricing`} component={PricingPage} />
+          <Route path={`${match.url}/reservation`} component={ReservationPage} />
+          <Route path={`${match.url}/about`} component={AboutPage} />
+          <Route path={`${match.url}/contact`} component={ContactPage} />
+          <Route component={NoMatchPage} />
+        </Switch>
+      </CSSTransition>
+    </Transition>
   );
 };
 
 Routes.propTypes = {
   match: PropTypes.object,
+  location: PropTypes.object,
 };
 
 export default Routes;
