5a72afb987e7f46fde6bdd611fd53ad1eb0df3bf
Arsalan Ahmed Ansari <arsalan.ansari@venturedive.com>
2018-12-17 21:44:24 +0500

    Upgraded @material-ui/core. Added About Page

--- src/modules/auth/about/AboutPage.js
+++ src/modules/auth/about/AboutPage.js
@@ -1,7 +1,58 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
-const AboutPage = () => {
-  return <div>About Page....</div>;
+const styles = () => ({
+  container: {
+    margin: 10,
+    marginLeft: '4%',
+    marginRight: '7%',
+    height: 483,
+  },
+  typography: {
+    marginTop: 30,
+  },
+  column: {
+    marginTop: '4%',
+    textAlign: 'justify',
+  },
+});
+
+const AboutPage = ({ classes }) => {
+  return (
+    <div className={`row ${classes.container}`}>
+      <div className={`col-md-5 ${classes.column}`}>
+        <img src="/src/static/images/About-C-bg.jpg" alt="" height="375" width="500" />
+      </div>
+      <div className={`col-md-2 ${classes.aboutImage}`}>
+        <img src="/src/static/images/about.png" alt="" />
+      </div>
+      <div className={`col-md-5 ${classes.column}`}>
+        <Typography variant="h4" gutterBottom>
+          About Us
+        </Typography>
+        <Typography className={classes.typography} variant="body2" gutterBottom>
+          body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
+          unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate,
+          numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam. Quos blanditiis
+          tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus,
+          cupiditate,numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+        </Typography>
+        <Typography className={classes.typography} variant="body2" gutterBottom>
+          body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
+          unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate,
+          numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam. Quos blanditiis
+          tenetur unde suscipit, quam beatae rerum inventore consectetur, neque doloribus,
+          cupiditate,numquam dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
+        </Typography>
+      </div>
+    </div>
+  );
 };
 
-export default AboutPage;
+AboutPage.propTypes = {
+  classes: PropTypes.object,
+};
+
+export default withStyles(styles)(AboutPage);
